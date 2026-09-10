d5ac64c8d97dc90d61ef21ed820c3a32aa70286d
unya-san <unyaunyatube@gmail.com>
2026-09-10 10:27:30 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://unya-san.github.io/my-build/',
+  site: 'https://unya-san.github.io',
   base: 'my-build'
 });
